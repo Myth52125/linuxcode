9f56603ae0ce67172e2851f1c213028d80ccf134
Myth52125 <myth52125@hotmail.com>
2017-10-14 15:38:37 +0800

    强弱引用

---
+++ extern/a.c
@@ -0,0 +1,9 @@
+ int global=1;
+// extern int b;
+
+
+// int fun()
+// {
+//     b++;
+//     return b;
+// }
---
+++ extern/b.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+extern int global;
+int main()
+{
+    global++;
+    printf("sizeof global: %d \n",sizeof(global));
+}
